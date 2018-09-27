666768bf192851bd9bb1595240cdf4e7eea89055
Carlos Castro Vargas <carlos.castro.vargas@gmail.com>
2018-09-27 11:56:13 +0300

    Implement recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,10 @@ class RecipesController < ApplicationController
   before_action :validate_params
 
   def search
+    url = generate_url
+    response = Faraday.get(url)
+    recipes = JSON.parse(response.body).fetch('results', []).map { |recipe| Recipe.new(recipe) }
+    render json: recipes
   end
 
   private
@@ -11,5 +15,9 @@ class RecipesController < ApplicationController
       if !@recipeSearch.valid?
         render json: @recipeSearch.errors, status: 400
       end
+    end
+
+    def generate_url        
+      "http://www.recipepuppy.com/api/?p=#{@recipeSearch.page}&q=#{@recipeSearch.query}"
     end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,9 @@
 class Recipe
   attr_accessor :title, :href, :ingredients, :thumbnail
-  
-  def initialize(title, href, ingredients, thumbnail)
-    @title = title
-    @href = href
-    @ingredients = ingredients
-    @thumbnail = thumbnail
+  def initialize(args = {})
+    args.each do |name, value|
+      attr_name = name.to_s.underscore
+      send("#{attr_name}=", value) if respond_to?("#{attr_name}=")
+    end
   end
 end
